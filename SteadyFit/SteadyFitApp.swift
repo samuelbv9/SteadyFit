26a7917ed1c2cfbcafe54a3ff99732042286f690
samuelbv9 <sbecharv@gmail.com>
2024-10-07 11:42:15 -0400

    restructure files

---
+++ SteadyFit/SteadyFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteadyFitApp.swift
+//  SteadyFit
+//
+//  Created by Samuel Bechar on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SteadyFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
