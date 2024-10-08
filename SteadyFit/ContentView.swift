ff429468977b41ed0131d414bd72c44e633c4550
samuelbv9 <sbecharv@gmail.com>
2024-10-08 18:05:56 -0400

    add authentication code

--- SteadyFit/ContentView.swift
+++ SteadyFit/ContentView.swift
@@ -6,16 +6,87 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var userIsLoggedIn = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if userIsLoggedIn {
+            otherPage
+        } else {
+            content
         }
-        .padding()
+    }
+    
+    var otherPage: some View {
+        VStack {
+            Text("Logged In")
+            Button {
+                logout()
+            } label: {
+                Text("Log Out")
+            }
+        }
+    }
+    
+    var content: some View {
+        VStack {
+            TextField("Email", text: $email)
+            
+            SecureField("Password", text: $password)
+            
+            Button {
+                register()
+            } label: {
+                Text("Sign Up")
+            }
+            
+            Button {
+                login()
+            } label: {
+                Text("Already have an account? Login")
+            }
+
+        }
+        .frame(width: 350)
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error != nil {
+                // show some popup maybe?
+                print(error!.localizedDescription)
+            }
+            else {
+                userIsLoggedIn = true
+            }
+        }
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if error != nil {
+                // show some popup maybe?
+                print(error!.localizedDescription)
+            }
+            else {
+                userIsLoggedIn = true
+            }
+            
+        }
+    }
+    
+    func logout() {
+        do {
+            try Auth.auth().signOut()
+            userIsLoggedIn = false
+        }
+        catch {
+            print("Failed to sign out: \(error.localizedDescription)")
+        }
+            
     }
 }
 
--- SteadyFit/SteadyFitApp.swift
+++ SteadyFit/SteadyFitApp.swift
@@ -6,22 +6,14 @@
 //
 
 import SwiftUI
-import FirebaseCore
-import FirebaseAuth
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
-
+import Firebase
 
 @main
 struct SteadyFitApp: App {
-    // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
