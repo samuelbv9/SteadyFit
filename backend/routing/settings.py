871c7ec4dc5fa506f6205d5e8cec05fe4e79dce3
Nilisha Baid <52362936+nilishabaid@users.noreply.github.com>
2024-10-10 14:12:00 -0400

    added db setup

--- backend/routing/settings.py
+++ backend/routing/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'routing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steadyfitdb',
+        'USER': 'steadyfit',
+        'PASSWORD': 'eecs441project',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
