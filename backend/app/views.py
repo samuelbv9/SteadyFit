3e8f6360965efd2c7f1638fce10018817b0fa324
yalhanuma <alhanuma@umich.edu>
2024-10-16 01:04:17 -0400

    for testing

--- backend/app/views.py
+++ backend/app/views.py
@@ -13,22 +13,33 @@ def goal(request):
     """
     if request.method != 'GET':
         return HttpResponse(status=404)
+    
+    # # approach #1
+    # json_data = json.loads(request.body)
+    # game_code = json_data["game_code"]
+    # user_id = json_data["user_id"]
+
+    # approach 2
+    game_code = request.GET.get("game_code")
+    user_id = request.GET.get("user_id")
+
+    if not game_code or not user_id:
+        return HttpResponse(status=400)
 
     cursor = connection.cursor()
-    json_data = json.loads(request.body)
-    game_code = json_data["game_code"]
-    user_id = json_data["user_id"]
 
-    cursor.execute("SELECT totalDistance, totalFrequency FROM GameParticipants WHERE gameCode = %s AND userId = %s", (game_code,user_id))
+    cursor.execute("SELECT totalDistance, totalFrequency FROM GameParticipants WHERE gameCode = %s AND userId = %s", (game_code, user_id))
     goal = cursor.fetchone()
-    cursor.execute("SELECT exerciseType, frequency, duration, FROM Games WHERE gamecode = %s", (game_code))
+    
+    cursor.execute("SELECT exerciseType, frequency, distance FROM Games WHERE gameCode = %s", (game_code,))
     gameInfo = cursor.fetchone()
+
     response_data = {
                         "exerciseType": gameInfo[0],
-                        "currentDistance": goal[0],
-                        "currentFrequency": goal[1], 
-                        "totalDistance": gameInfo[2],
-                        "totalFrequency": gameInfo[1]
+                        "currentDistance": goal[0], # may be null
+                        "currentFrequency": goal[1], # may be null
+                        "totalDistance": gameInfo[2], # may be null
+                        "totalFrequency": gameInfo[1] # may be null
                     }
 
     return JsonResponse(response_data)
@@ -42,6 +53,14 @@ def past_games(request):
     pass
 
 def active_games(request):
+    if request.method != 'GET':
+        return HttpResponse(status=404)
+
+    cursor = connection.cursor()
+
+    pass
+
+def game_details(request):
     if request.method != 'GET':
         return HttpResponse(status=404)
 
@@ -63,9 +82,14 @@ def create_game(request):
     cursor = connection.cursor()
     json_data = json.loads(request.body)
 
+    # check for duplicate game codes
+    # while cursor.execute("SELECT * FROM Games WHERE gameCode = %s", (game_code,)).fetchall():
+    #     game_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
     game_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
-    while cursor.execute("SELECT * FROM Games WHERE gameCode = %s", (game_code,)).fetchall():
+    cursor.execute("SELECT * FROM Games WHERE gameCode = %s", (game_code,))
+    while cursor.fetchall():  
         game_code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+        cursor.execute("SELECT * FROM Games WHERE gameCode = %s", (game_code,))
 
     user_id, bet_amount, exercise_type, frequency, \
     distance, duration, adaptive_goals, start_date = (
@@ -74,18 +98,19 @@ def create_game(request):
             "distance", "duration", "adaptive_goals", "start_date"
         ]
     )
-
+    
     # Add game to Games table
-    cursor.execute("INSERT INTO Games (betAmount, exerciseType, frequency, \
+    cursor.execute("INSERT INTO Games (gameCode, betAmount, exerciseType, frequency, \
                    distance, duration, adaptiveGoals, startDate) \
                    VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                   (bet_amount, exercise_type, frequency, distance,
+                   (game_code, bet_amount, exercise_type, frequency, distance,
                     duration, adaptive_goals, start_date))
 
     # Set current user as player of this game
-    cursor.execute("INSERT INTO GameParticipants (gameCode, userID) VALUES (%s, %s)",
+    cursor.execute("INSERT INTO GameParticipants (gameCode, userId) VALUES (%s, %s)",
                    (game_code, user_id))
 
+    # should we return some type of confirmation details or the game code so the frontend has some feedback?
     return JsonResponse({})
 
 
@@ -117,10 +142,13 @@ def bet_details(request):
     json_data = json.loads(request.body)
     game_code = json_data["game_code"]
 
-    cursor.execute("SELECT userID, balance FROM GameParticipants WHERE gameCode = %s", (game_code,))
+    if not game_code:
+        return HttpResponse(status=400)        
+
+    cursor.execute("SELECT userId, balance FROM GameParticipants WHERE gameCode = %s", (game_code,))
     participants = cursor.fetchall()
     response_data = [
-        {"userID": row[0], "balance": row[1]}
+        {"userId": row[0], "balance": row[1]}
         for row in participants
     ]
 
