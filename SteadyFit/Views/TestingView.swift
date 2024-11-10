a1fbfd1e1e3659eca05854ce333ddafe56ee8aa7
Debbie Shih <dcjshih@umich.edu>
2024-11-10 18:35:17 -0500

    ADD: Homepage stuff

--- SteadyFit/Views/TestingView.swift
+++ SteadyFit/Views/TestingView.swift
@@ -8,21 +8,39 @@
 import SwiftUI
 
 struct TestingView: View {
-    var body: some View {
-        Text("Testing: bet_details...")
-        Button(action: {
-            GamesStore.shared.getBetBalances("GAME001")
-        }) {
-            HStack {
-                Text("Test GET /bet_details/ ")
-                    .fontWeight(.medium)
-                Image(systemName: "arrow.right")
-                    .resizable()
-                    .frame(width: 25, height: 25)
+    // Observe the shared instance of GamesStore
+        @ObservedObject var gamesStore = GamesStore.shared
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text("Welcome")
+                    .font(.largeTitle)
                     .padding()
+                Text("Upcoming Tasks")
+                // ForEach loop to display each game's details
+                List(gamesStore.activeGames, id: \.gameCode) { game in
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Game Code: \(game.gameCode)")
+                        Text("Exercise Type: \(game.exerciseType)")
+                        Text("Bet Amount: \(game.betAmount)")
+                        Text("Frequency: \(game.frequency ?? 0)")
+                        Text("Distance: \(game.distance ?? 0.0)")
+                        Text("Frequency Goal: \(game.frequencyGoal ?? 0)")
+                        Text("Distance Goal: \(game.distanceGoal ?? 0.0)")
+                        Text("Duration: \(game.duration)")
+                        Text("Start Date: \(game.startDate)")
+                    }
+                    .padding()
+                }
+            }
+            .padding()
+            .onAppear {
+                Task {
+                    // Replace with the actual userId
+                    await gamesStore.getActiveGames(userId: "766a6bf7-c4f2-4bfb-abb4-b9aa63f435e8")
+                }
             }
         }
-    }
 }
 
 //#Preview {
